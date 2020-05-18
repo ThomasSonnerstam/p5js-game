d5ea253206e006d95376c5565069c81964c513db
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-05-18 10:22:41 +0200

    Updates: - Fixed jumping function bug - Fixed colliders

--- index.js
+++ index.js
@@ -3,37 +3,28 @@ let obstacle;
 let GRAVITY = 1;
 let JUMP = 20;
 let bg;
-let obstacles = [];
 
 function setup() {
-  obstacles.push(obstacle)
+  frameRate(60);
+  // Canvas size
   createCanvas(windowWidth, windowHeight);
+  // Player sprite & collider
   player = createSprite(50, windowHeight - 400, 20, 20);
   player.addAnimation("default", "assets/sprites/ghost-right.png");
   player.setCollider("circle", 0, 0, 20);
+  // Obstacle sprite(s) and collider(s)
   obstacle = createSprite(600, windowHeight - 35, 50, 70);
   obstacle.shapeColor = color(255, 123, 0);
   obstacle.setCollider("rectangle");
-
-
-  frameRate(60);
+  // Background image
   bg = loadImage("/assets/sprites/supermario.png");
-
-
-  // player.setDefaultCollider();
-  // obstacle.setDefaultCollider();
 }
   
 function draw() {
-  
   background(bg);
   
   player.velocity.y += GRAVITY;
   player.velocity.x = 0;
-  
-  obstacle.velocity.x = player.velocity.x;
-
-
 
   // Stops the player vertical speed if the player reaches the bottom of the canvas
   if (player.y >= windowHeight - 25) {
@@ -68,18 +59,10 @@ function draw() {
 
 // Makes the player jump
 function keyPressed() {
-  // if (player.y >= windowHeight - 25 && keyCode === UP_ARROW) {
-  //   player.velocity.y = -JUMP;
-  // }
-  
-  console.log(player.touching);
-  
-  
 
   if ((player.velocity.y === 0 || player.velocity.y === 1) && keyCode === UP_ARROW) {
     player.velocity.y = -JUMP;
   }
-  
 }
 
 

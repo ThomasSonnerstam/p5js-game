9c538462f19f127e4bcfcc577e7233068c49b6ca
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2020-05-21 16:28:10 +0200

    Updates: - Added background music - Added some new buttons

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ let spear;
 let spearCatcher;
 let spearSingle;
 let isPlaying = false;
+let musicOn = true;
 const startScreen = document.querySelector(".start-screen");
 const gameOver = document.querySelector(".game-over");
 const playAgain = document.querySelector(".play-again");
 const startOver = document.querySelector(".start-over");
 const startBtn = document.querySelector(".start-game");
+const musicBtn = document.querySelector(".musicBtn");
+
 
 function startGame() {
   score = 0;
@@ -84,19 +87,19 @@ function startGame() {
 
   rockObstacle = createSprite(1360, windowHeight - 100.5, 100, 100); // Displaced block bottom right corner
   obstacle16 = createSprite(200, windowHeight - 500, 100, 10);
-  obstacle16.shapeColor = "black";
+  obstacle16.shapeColor = "#765A45";
   obstaclesGroup.add(obstacle16);
   obstacle19 = createSprite(300, windowHeight - 650, 100, 10);
-  obstacle19.shapeColor = "black";
+  obstacle19.shapeColor = "#765A45";
   obstaclesGroup.add(obstacle19);
   obstacle21 = createSprite(600, windowHeight - 600, 70, 10);
-  obstacle21.shapeColor = "black";
+  obstacle21.shapeColor = "#765A45";
   obstaclesGroup.add(obstacle21);
   obstacle24 = createSprite(1050, windowHeight - 650, 130, 10);
-  obstacle24.shapeColor = "black";
+  obstacle24.shapeColor = "#765A45";
   obstaclesGroup.add(obstacle24);
   obstacle26 = createSprite(930, windowHeight - 600, 20, 10);
-  obstacle26.shapeColor = "black";
+  obstacle26.shapeColor = "#765A45";
   fakeBlock.add(obstacle26);
   obstacle24.rotation = -35;
 
@@ -105,7 +108,7 @@ function startGame() {
 
   // Obstacles sprites added to the collider group
   obstacles.forEach((item) => {
-    item.shapeColor = "black";
+    item.shapeColor = "#765A45";
     obstaclesGroup.add(item);
   });
 
@@ -120,7 +123,7 @@ function startGame() {
 
   spearInterval = setInterval(() => {
     spearSingle = createSprite(50, windowHeight - 400, 60, 5);
-    spearSingle.shapeColor = "red";
+    spearSingle.shapeColor = "#F47550";
     spearSingle.velocity.x = 5;
     spear.add(spearSingle);
     obstaclesGroup.add(spearSingle);
@@ -151,12 +154,15 @@ function preload() {
   jumpLeft = loadAnimation("assets/sprites/ghost-left-jump.png");
   bg = loadImage("/assets/sprites/background.png");
   sound = new Audio("assets/sounds/coin.mp3");
+  bgmusic = new Audio("assets/sounds/bgmusic.mp3")
 }
 
 function setup() {
   frameRate(60);
   // Canvas size
   createCanvas(windowWidth, windowHeight);
+
+  bgmusic.play();
 
   if (!isPlaying) {
     textSize(48);
@@ -172,7 +178,7 @@ function draw() {
     textSize(35);
     text(`Score: ${score}`, width - 300, 40);
 
-    if (score === 1) {
+    if (score === 10) {
       isPlaying = false;
       gameOver.classList.add("show");
     }
@@ -277,4 +283,17 @@ startBtn.addEventListener("click", () => {
   isPlaying = true;
   startScreen.classList.add("hidden");
   startOver.classList.remove("hidden");
+  musicBtn.classList.remove("hidden");
+})
+
+musicBtn.addEventListener("click", () => {
+  if (musicOn) {
+     bgmusic.pause();
+     musicBtn.innerHTML = "Sound on";
+     musicOn = false;
+  } else {
+    bgmusic.play();
+    musicBtn.innerHTML = "Sound off";
+    musicOn = true;
+  }
 })
